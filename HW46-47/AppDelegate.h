bfebe1b4601de957ca4b781ccaf7c20ca9c0c153
Ilya Egorov <i.egorov@pediatr247.ru>
2016-06-29 17:55:53 +0300

    Initial Commit

---
+++ HW46-47/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW46-47
+//
+//  Created by Илья Егоров on 29.06.16.
+//  Copyright © 2016 Ilya Egorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW46-47/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW46-47
+//
+//  Created by Илья Егоров on 29.06.16.
+//  Copyright © 2016 Ilya Egorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
